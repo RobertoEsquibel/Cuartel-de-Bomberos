813bfafa67fe0811bf07900729f8e31be31ed766
Eliana-Ponce <elianaponce0202@gmail.com>
2023-10-03 21:43:45 -0300

    entidades bombero

--- src/entidades/Bombero.java
+++ src/entidades/Bombero.java
@@ -1,14 +1,91 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
-* and open the template in the editor.
- */
+
 package entidades;
 
-/**
- *
- * @author Roberto
- */
+import java.time.LocalDate;
+
 public class Bombero {
+    private int id_bombero;
+    private int dni;
+    private String nombre_ape;
+    private LocalDate fecha_nac;
+    private int celular;
+    private int codBrigada;
+
+    public Bombero() {
+    }
+
+    public Bombero(int dni, String nombre_ape, LocalDate fecha_nac, int celular, int codBrigada) {
+        this.dni = dni;
+        this.nombre_ape = nombre_ape;
+        this.fecha_nac = fecha_nac;
+        this.celular = celular;
+        this.codBrigada = codBrigada;
+    }
+
+    public Bombero(int id_bombero, int dni, String nombre_ape, LocalDate fecha_nac, int celular, int codBrigada) {
+        this.id_bombero = id_bombero;
+        this.dni = dni;
+        this.nombre_ape = nombre_ape;
+        this.fecha_nac = fecha_nac;
+        this.celular = celular;
+        this.codBrigada = codBrigada;
+    }
+
+    public int getId_bombero() {
+        return id_bombero;
+    }
+
+    public void setId_bombero(int id_bombero) {
+        this.id_bombero = id_bombero;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre_ape() {
+        return nombre_ape;
+    }
+
+    public void setNombre_ape(String nombre_ape) {
+        this.nombre_ape = nombre_ape;
+    }
+
+    public LocalDate getFecha_nac() {
+        return fecha_nac;
+    }
+
+    public void setFecha_nac(LocalDate fecha_nac) {
+        this.fecha_nac = fecha_nac;
+    }
+
+    public int getCelular() {
+        return celular;
+    }
+
+    public void setCelular(int celular) {
+        this.celular = celular;
+    }
+
+    public int getCodBrigada() {
+        return codBrigada;
+    }
+
+    public void setCodBrigada(int codBrigada) {
+        this.codBrigada = codBrigada;
+    }
+
+    @Override
+    public String toString() {
+        return "Bombero{" + "id_bombero=" + id_bombero + ", dni=" + dni + ", nombre_ape=" + nombre_ape + ", fecha_nac=" + fecha_nac + ", celular=" + celular + ", codBrigada=" + codBrigada + '}';
+    }
     
-}
+    }
+    
+    
+    
+
--- src/entidades/Brigada.java
+++ src/entidades/Brigada.java
@@ -1,14 +1,76 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package entidades;
 
-/**
- *
- * @author Roberto
- */
+
 public class Brigada {
+    private int codBrigada;
+    private String nombre;
+    private String especialidades;
+    private boolean libre;
+    private int nro_cuartel;
+
+    public Brigada() {
+    }
+
+    public Brigada(String nombre, String especialidades, boolean libre, int nro_cuartel) {
+        this.nombre = nombre;
+        this.especialidades = especialidades;
+        this.libre = libre;
+        this.nro_cuartel = nro_cuartel;
+    }
+
+    public Brigada(int codBrigada, String nombre, String especialidades, boolean libre, int nro_cuartel) {
+        this.codBrigada = codBrigada;
+        this.nombre = nombre;
+        this.especialidades = especialidades;
+        this.libre = libre;
+        this.nro_cuartel = nro_cuartel;
+    }
+
+    public int getCodBrigada() {
+        return codBrigada;
+    }
+
+    public void setCodBrigada(int codBrigada) {
+        this.codBrigada = codBrigada;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEspecialidades() {
+        return especialidades;
+    }
+
+    public void setEspecialidades(String especialidades) {
+        this.especialidades = especialidades;
+    }
+
+    public boolean isLibre() {
+        return libre;
+    }
+
+    public void setLibre(boolean libre) {
+        this.libre = libre;
+    }
+
+    public int getNro_cuartel() {
+        return nro_cuartel;
+    }
+
+    public void setNro_cuartel(int nro_cuartel) {
+        this.nro_cuartel = nro_cuartel;
+    }
+
+    @Override
+    public String toString() {
+        return "Brigada{" + "codBrigada=" + codBrigada + ", nombre=" + nombre + ", especialidades=" + especialidades + ", libre=" + libre + ", nro_cuartel=" + nro_cuartel + '}';
+    }
+    
     
 }
