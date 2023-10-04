de23e353d1c2862a25b31890b55f3892603cd8b5
Eliana-Ponce <elianaponce0202@gmail.com>
2023-10-03 21:01:23 -0300

    conexion

--- src/conexiondata/Conexion.java
+++ src/conexiondata/Conexion.java
@@ -10,8 +10,10 @@
 package conexiondata;
 
 import java.sql.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 import java.sql.DriverManager;
-import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -19,7 +21,33 @@ import java.sql.DriverManager;
 
 public class Conexion {
 
-        
+      private static final String URL="jdbc:mariadb://localhost/";
+  private static final String DB="bomberos";
+  private static final String USUARIO="root";
+  private static final String PASSWORD="";
+  private static Connection connection; 
+
+private Conexion(){}
+
+public static Connection getConexion (){
+    
+    if(connection==null){
+    try {
+    Class.forName("org.mariadb.jdbc.Driver");
+    
+    connection= DriverManager.getConnection(URL+DB,USUARIO,PASSWORD);
+    JOptionPane.showMessageDialog(null, "Conectado");
+    }catch (ClassNotFoundException ex){
+    JOptionPane.showMessageDialog(null,"Error al cargar los Driver");
+    }catch (SQLException ex){
+    JOptionPane.showMessageDialog(null,"Error al conectar a la base de datos");
+    }
+    }
+    return connection;
+    }
+
+}
+  
     
         
-}
+
